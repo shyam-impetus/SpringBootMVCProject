a209aa04a1e977858cca11beb874e24d9f32aee6
nidhiprajapati0820 <nidhi.khurpia@gmail.com>
2021-12-11 11:39:54 -0700

    Initial commit

---
+++ src/main/java/com/simplilearn/SpringBootMVCProject/SpringBootMvcProjectApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.SpringBootMVCProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcProjectApplication.class, args);
+	}
+
+}
